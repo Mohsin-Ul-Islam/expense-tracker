91d187d480e4f68ef758be5094b53c46456a4602
Mohsin-Ul-Islam <mohsinulislam180@gmail.com>
2021-09-30 02:08:39 +0500

    fix: pass self to test_version

--- tests/test_expense_tracker.py
+++ tests/test_expense_tracker.py
@@ -1,5 +1,12 @@
+from unittest import TestCase, main
+
 from expense_tracker import __version__
 
 
-def test_version():
-    assert __version__ == "0.1.0"
+class TestExpenseTracker(TestCase):
+    def test_version(self):
+        assert __version__ == "0.1.0"
+
+
+if __name__ == "__main__":
+    main()
